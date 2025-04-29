d97acb08748de3911f831cf70b989de4beea0a13
TatakAdi <tatakadisubagyo@gmail.com>
2025-04-29 23:13:07 +0800

    Email verification API Implement

--- src/app/(pages)/EmailVerif/page.jsx
+++ src/app/(pages)/EmailVerif/page.jsx
@@ -1,11 +1,38 @@
 "use client";
-import Image from "next/image";
 import EmailVerifBox from "@/components/EmailVerifBox";
+import { verifyEmail } from "@/lib/api/verifyEmail";
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function EmailVerif() {
   const searchParams = useSearchParams();
   const email = searchParams.get("email");
+  const token = searchParams.get("token");
+  const router = useRouter();
+
+  const [status, setStatus] = useState("loading"); // loading, success, error, idle
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (token) {
+      setStatus("loading");
+      verifyEmail(token).then((res) => {
+        if (res.succes) {
+          setStatus("success");
+          setMessage(res.message);
+          setTimeout(() => {
+            router.push("/Login");
+          }, 2000);
+        } else {
+          setStatus("error");
+          setMessage(res.message);
+        }
+      });
+    } else {
+      setStatus("idle");
+    }
+  }, [token, router]);
 
   return (
     <div className="w-full h-screen flex justify-center items-center">
