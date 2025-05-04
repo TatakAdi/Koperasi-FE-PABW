8774414db8e81185ee15a4b22862987c0ace327b
TatakAdi <tatakadisubagyo@gmail.com>
2025-05-04 22:18:45 +0800

    Side Panel UI

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,10 +6,15 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import SidePanel from "./components/SidePanel";
+import useInput from "./hooks/useInput";
 
 export default function Home() {
   const [authUser, setAuthUser] = useState(null);
   const [category, setCategory] = useState("food"); // Kategori : "food","Snack","drink"
+  const [minPrice, setMinPrice] = useInput();
+  const [maxPrice, setMaxPrice] = useInput();
+  const [sellSort, setSellSort] = useState("Terbanyak");
+  const [keyword, setKeyword] = useInput();
   const router = useRouter();
 
   useEffect(() => {
@@ -34,9 +39,19 @@ export default function Home() {
 
   return (
     <div className="min-h-screen w-full font-[family-name:var(--font-geist-sans)] ">
-      <Navbar />
+      <Navbar keyword={keyword} onKeywordCahnge={setKeyword} />
       <main>
-        <SidePanel category={category} />
+        <SidePanel
+          category={category}
+          onCategoryChange={setCategory}
+          salesSort={sellSort}
+          setSalesSort={setSellSort}
+          maxPrice={maxPrice}
+          minPrice={minPrice}
+          onMaxPriceChange={setMaxPrice}
+          onMinPriceChange={setMinPrice}
+        />
+        <div className="grid grid-cols-4"></div>
       </main>
     </div>
   );
