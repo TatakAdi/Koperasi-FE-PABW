55c29c2ca6f1e20d114dd7aa53e4790e45b5e386
TatakAdi <tatakadisubagyo@gmail.com>
2025-05-06 20:49:20 +0800

    Prepare fix : OTP Code

--- src/app/(pages)/OTPCode/page.jsx
+++ src/app/(pages)/OTPCode/page.jsx
@@ -5,7 +5,7 @@ import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export default function EmailVerif() {
+export default function OTPCode() {
   const searchParams = useSearchParams();
   const email = searchParams.get("email");
   const token = searchParams.get("token");
